f528980097fdee6a4e41148796768340bb5538cc
samanmovaghar <saman.movaghar@omegaponit.se>
2020-06-16 11:37:02 +0200

    Define an async function that fetches characters from another endpoint and passed as props in the child component.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,69 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 import swapiFilms from "./api/swapiFilms";
 import MovieList from "./components/MovieList/MovieList";
 import MovieDetails from "./components/MovieDetails/MovieDetails";
+import Spinner from "./components/ShareComponents/Spinner/Spinner";
 
 const App = () => {
   const [movies, setMovies] = useState([]);
-  const [details, setDetails] = useState({});
+  const [details, setDetails] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [characters, setCharacters] = useState([]);
+  const [isShowing, setIsShowing] = useState(false);
+
   useEffect(() => {
     getMovies();
   }, []);
-
+  // the async function that called swapiFilms and set the state fot fetch movies.
   const getMovies = async () => {
-    // the async function that called swapiFilms and set the state fot fetch movies.
-    const { data } = await swapiFilms();
-    console.log(data);
-    setMovies(data.results);
+    try {
+      setIsLoading(true);
+      const { data } = await swapiFilms();
+      setMovies(data.results);
+      setIsLoading(false);
+    } catch (error) {
+      console.error("Error to fetch the star wars movies", error);
+      setIsLoading(false);
+      setIsError(true);
+    }
   };
   // create function when the user click to get details of movie
-  const movieClickHandler = (movie) => {
-    setDetails(movie);
-    console.log(movie);
+  const movieClickHandler = async (movie) => {
+    try {
+      setDetails(movie);
+      setIsLoading(true);
+      for await (const dataCharacters of movie.characters) {
+        const response = await axios.get(dataCharacters);
+        // add resultet response as new array
+        setCharacters((characters) => [...characters, response.data]);
+      }
+      setIsLoading(false);
+      setIsShowing(!isShowing);
+    } catch (error) {
+      console.error("Error to fetch the charactersP", error);
+      setIsLoading(false);
+      setIsError(true);
+    }
   };
+
+  const closeModal = () => {
+    setDetails(null);
+    setIsShowing(false);
+  };
+
   return (
     <>
       <MovieList movies={movies} movieClick={movieClickHandler} />
-      <MovieDetails movie={details} />
+      <MovieDetails
+        movie={details}
+        onClose={closeModal}
+        isShowing={isShowing}
+        actors={characters}
+      />
+      {isLoading && <Spinner />}
+      {isError && <div>Error to load movies.</div>}
     </>
   );
 };
--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -1,7 +1,50 @@
 import React from "react";
+import ReactDOM from "react-dom";
+import * as _ from "lodash";
+import closeIcon from "../../assets/icons/close.svg";
+import "./MovieDetails.css";
 
-const MovieDetails = ({ movie }) => {
-   return <div>{movie.title}</div>;
+const MovieDetails = ({ movie, onClose, isShowing, actors }) => {
+  return isShowing
+    ? ReactDOM.createPortal(
+        <div>
+          <div className="modal-overlay" />
+          <div
+            className="modal-wrapper"
+            aria-modal
+            aria-hidden
+            tabIndex={-1}
+            role="dialog"
+          >
+            <div className="modal">
+              <div className="modal-header">
+                <h1 className="modal-title"> {movie.title} </h1>
+                <img
+                  className="closeButton"
+                  onClick={onClose}
+                  aria-label="Close"
+                  alt="close"
+                  src={closeIcon}
+                />
+              </div>
+
+              <div>
+                <div className="card">
+                  <div className="serial-info-row">
+                    <div>
+                      {_.sortBy(actors, "name").map((x) => {
+                        return <div key={x.created}> {x.name}</div>;
+                      })}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>,
+        document.body
+      )
+    : null;
 };
 
 export default MovieDetails;
